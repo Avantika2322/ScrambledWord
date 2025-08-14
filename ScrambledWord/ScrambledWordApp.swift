477a83ed25aa2bb12e34a58eeab63ca7b667239d
Binary Semantics <binarysemantics@Binarys-Mac-mini.local>
2025-08-14 16:11:10 +0530

    Initail

---
+++ ScrambledWord/ScrambledWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambledWordApp.swift
+//  ScrambledWord
+//
+//  Created by Binary Semantics on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrambledWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScrambledWordTests/ScrambledWordTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScrambledWordTests.swift
+//  ScrambledWordTests
+//
+//  Created by Binary Semantics on 14/08/25.
+//
+
+import Testing
+
+struct ScrambledWordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
